e306ce24524c5d0d9cb7ebd7bf2594f494e16eb1
Jace Babin <jbabin91@gmail.com>
2022-02-07 16:15:34 -0700

    Cleaned up the Program file and moved everything into Registrars.

---
+++ CwkSocial.Api/Extensions/RegistrarExtensions.cs
@@ -0,0 +1,28 @@
+﻿using CwkSocial.Api.Registrars;
+
+namespace CwkSocial.Api.Extensions;
+
+public static class RegistrarExtensions
+{
+    public static void RegisterServices(this WebApplicationBuilder builder, Type scanningType)
+    {
+        foreach (var registrar in GetRegistrars<IWebApplicationBuilderRegistrar>(scanningType))
+        {
+            registrar.RegisterServices(builder);
+        }
+    }
+    public static void RegisterPipelineComponents(this WebApplication app, Type scanningType)
+    {
+        foreach (var registrar in GetRegistrars<IWebApplicationRegistrar>(scanningType))
+        {
+            registrar.RegisterPipelineComponents(app);
+        }
+    }
+    private static IEnumerable<T> GetRegistrars<T>(Type scanningType) where T: IRegistrar
+    {
+        return scanningType.Assembly.GetTypes()
+            .Where(t => t.IsAssignableTo(typeof(T)) && !t.IsAbstract && !t.IsInterface)
+            .Select(Activator.CreateInstance)
+            .Cast<T>();
+    }
+}
